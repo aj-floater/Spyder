ee8615af48fbd5d5c76cda7b3a18de89557dd0a1
Archie James <arjames1618@gmail.com>
2023-05-28 00:36:43 +0100

    first commit maybe?

--- include/get_path.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-namespace MyPaths {
-  std::string getExecutablePath();
-  std::string getExecutableDir();
-  std::string mergePaths(std::string pathA, std::string pathB);
-  // bool checkIfFileExists (const std::string& filePath);
-}
--- src/property_manager.cpp
+++
@@ -1,5 +0,0 @@
-#include "property_manager.h"
-
-float PropertyManager::directional_light[3] = {-0.2f, -1.0f, -0.3f};
-bool PropertyManager::invalid = false;
-float PropertyManager::delta_time;
